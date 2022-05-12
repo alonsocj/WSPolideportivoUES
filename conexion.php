d97afeb592f6eb9a09fb9c73a31b41011f258b8b
alonsocj <cornejo.aj98@hotmail.com>
2022-05-11 21:45:24 -0600

    first commit

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+function conectar() {
+    $user = "";
+    $pass = "";
+    $server = "";
+    $db = "";
+    $conexion = mysqli_connect($server, $user, $pass, $db) or die ("Error al conectar a la base de datos".mysql_error());
+    mysql_select_db($db, $conexion) or die ("Error al seleccionar la base de datos".mysql_error());
+    return $conexion;
+?>
