93f1e4ad36d6ee7434ec569ec90359ed7d4199a8
ChristianG0816 <chrisgue0819@gmail.com>
2022-05-22 14:44:22 -0600

    Se realiza el Webservice de Consultar evento

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 function conectar() {
-    $user = "";
+    $user = "root";
     $pass = "";
-    $server = "";
-    $db = "";
+    $server = "localhost";
+    $db = "polideportivog10";
     $conexion = mysql_connect($server, $user, $pass, $db) or die ("Error al conectar a la base de datos".mysql_error());
     mysql_select_db($db, $conexion) or die ("Error al seleccionar la base de datos".mysql_error());
     return $conexion;
