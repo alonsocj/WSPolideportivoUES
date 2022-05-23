6323df602922b86544c75c53d9a40b1d3a7c6f0b
alonsocj <cornejo.aj98@hotmail.com>
2022-05-23 10:41:21 -0600

    web service insertar evento y actualizar disponibilidad de local completos

--- ws_evento_insert.php
+++ ws_evento_insert.php
@@ -1,5 +1,15 @@
 <?php
+$idevento = $_REQUEST['idevento'];
+$idtipoe = $_REQUEST['idtipoe'];
+$nomevento = $_REQUEST['nomevento'];
+$costoevento = $_REQUEST['costoevento'];
+$cantidadautorizada = $_REQUEST['cantidadautorizada'];
 include('conexion.php');
-$con=conectar();
+$con = conectar();
 
-?>
+$sql = "INSERT INTO evento (idevento, idtipoe, nomevento, costoevento, cantidadautorizada) VALUES ('$idevento', '$idtipoe', '$nomevento', '$costoevento', '$cantidadautorizada')";
+$resultado = mysqli_query($con, $sql) or die(mysqli_error($con));
+if (mysqli_affected_rows($con) == 1)
+    $respuesta = array('resultado' => 1);
+echo json_encode($respuesta);
+$con->close();
--- ws_local_update.php
+++ ws_local_update.php
@@ -1,5 +1,12 @@
 <?php
+$idlocal = $_REQUEST['idlocal'];
+$idhorario = $_REQUEST['idhorario'];
+$disponibilidad = $_REQUEST['disponibilidad'];
 include('conexion.php');
-$con=conectar();
-
-?>
+$con = conectar();
+$sql = "UPDATE horarioslocales SET disponibilidad = '$disponibilidad' WHERE idhorario = '$idhorario' AND idlocal = '$idlocal'";
+$resultado = mysqli_query($con, $sql) or die(mysqli_error($con));
+if (mysqli_affected_rows($con) == 1)
+    $respuesta = array('resultado' => 1);
+echo json_encode($respuesta);
+$con->close();
