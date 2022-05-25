92c462aebcb206f10cb7324ae698d9bd1fefee0c
williamVM4 <85971941+williamVM4@users.noreply.github.com>
2022-05-25 14:38:26 -0600

    Update ws_reservacion_query.php

--- ws_reservacion_query.php
+++ ws_reservacion_query.php
@@ -4,7 +4,7 @@ $month=$_REQUEST['month'];
 $day=$_REQUEST['day'];
 include('conexion.php');
 $con=conectar();
-$registros=mysql_query("Select * from reservacion where FECHAREGISTRO> ".$year."-".$month."-".$day."",$con) or
+$registros=mysql_query("Select * from reservacion where FECHAREGISTRO ='".$year."-".$month."-".$day."'",$con) or
 die("Problemas en el select:".mysql_error());
 $filas=array();
 while ($reg=mysql_fetch_assoc($registros))
