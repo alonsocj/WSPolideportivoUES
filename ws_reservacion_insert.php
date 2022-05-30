da7fc43f85dae8d5fe77eb765ab9ae248167c3e7
CarolinaIPD <75870831+CarolinaIPD@users.noreply.github.com>
2022-05-30 12:47:25 -0600

    Update ws_reservacion_insert.php

--- ws_reservacion_insert.php
+++ ws_reservacion_insert.php
@@ -12,26 +12,13 @@ $fechaRegistro=$_REQUEST['FECHAREGISTRO'];
 include('conexion.php');
 $con=conectar();
 
-$servidor="localhost";
-$usuario="id19008253_grupo10";
-$password="|y|7N<u4MKVL!O*R";
-$baseDatos="id19008253_polideportivoues";
-
-$respuesta=array('resultado'=>0);
-json_encode($respuesta);
-
-$conexion=mysql_connect($servidor,$usuario,$password) or
-die ("Problemas en la conexion");
-mysql_select_db($baseDatos,$conexion)
- or die("Problemas en la seleccion de la base de datos");
-
 $query = "INSERT INTO RESERVACION VALUES('".$idReservacion."','".$idCobro."','".$idPersona."','".$idTipoR."','".$idEvento."','".$idPeriodoReserva."','".$idHorario."','".$idLocal."','".$fechaRegistro."');";
 
-$resultado = mysql_query($query) or die(mysql_error());
+$resultado = mysql_query($con, $query) or die(mysql_error($con));
 
 //Si la respuesta es correcta enviamos 1 y sino enviamos 0
-if(mysql_affected_rows() == 1)
+if(mysql_affected_rows($con) == 1)
  $respuesta=array('resultado'=>1);
 echo json_encode($respuesta);
-mysql_close($conexion);
+$con->close();
 ?>
