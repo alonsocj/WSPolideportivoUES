6f100bb42698542ff1a1c0bb100d4564c73c1202
CarolinaIPD <75870831+CarolinaIPD@users.noreply.github.com>
2022-05-30 13:05:20 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 function conectar() {
-    $user = "root";
-    $pass = "";
+    $user = "id19008253_grupo10";
+    $pass = "|y|7N<u4MKVL!O*R";
     $server = "localhost";
-    $db = "polideportivo";
-    $conexion = mysqli_connect($server, $user, $pass, $db) or die ("Error al conectar a la base de datos".mysqli_error($conexion));
-    mysqli_select_db($conexion,$db) or die ("Error al seleccionar la base de datos".mysqli_error($conexion));
+    $db = "id19008253_polideportivoues";
+    $conexion = mysql_connect($server, $user, $pass, $db) or die ("Error al conectar a la base de datos".mysql_error());
+    mysql_select_db($db,$conexion) or die ("Error al seleccionar la base de datos".mysql_error());
     return $conexion;
 }
